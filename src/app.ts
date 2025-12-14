ebe1afb50b93bbe8e8d94a448dbbd43fc997e3f1
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-12-14 17:30:59 -0300

    separando back do front em diferentes repositorios

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes";
+import { connectDB } from "./database";
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: "frontminiprojeto.joaoguimaraes.dev",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", cors());
+
+
+app.options("*", cors());
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+});
+
+app.use("/", router);
+
+connectDB();
+
+export default app;
